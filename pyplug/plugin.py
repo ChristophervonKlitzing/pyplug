c2af27a9e651abe5ca209fe0ca0410c31dbbfa0a
Christopher von Klitzing <christopherklitzing@gmail.com>
2024-09-14 23:59:56 +0200

    Small cleanups

--- pyplug/plugin.py
+++ pyplug/plugin.py
@@ -1,8 +1,6 @@
 from abc import ABC, abstractmethod
-from typing import TYPE_CHECKING
 
-if TYPE_CHECKING:
-    from pyplug.registry_state import RegistryState
+from pyplug.registry_state import RegistryState
 
 
 class Plugin(ABC):
