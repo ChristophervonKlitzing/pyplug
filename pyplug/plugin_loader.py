a672a368cb62e407217d32dac0a5f24582751e1e
Christopher von Klitzing <christopherklitzing@gmail.com>
2024-09-14 11:07:17 +0200

    Improve ModulePluginLoader

--- pyplug/plugin_loader.py
+++ pyplug/plugin_loader.py
@@ -36,17 +36,32 @@ class ModulePluginLoader(PluginLoader):
         self._plugin_name = self._directory_path.replace(".", "_")
 
         self._module = None
+        self._plugin_class = None
 
     def load(self):
         if self.is_loaded():
             return 
 
+        self._module = self._get_or_import_module()
+        self._plugin_class = self._find_plugin_class()
+
+    def unload(self):
+        self._module = None
+        self._plugin_class = None
+
+    def is_loaded(self) -> bool:
+        return self._module is not None and self._plugin_class is not None
+    
+    def _get_or_import_module(self):
         if not os.path.exists(self._module_path):
             raise RuntimeError(f"Module '{self._module_path}' is not existing")
 
         if self._plugin_name not in sys.modules:
             # Create the module spec
             spec = util.spec_from_file_location(self._plugin_name, self._module_path, submodule_search_locations=[])
+
+            if spec is None:
+                raise RuntimeError(f"Could not create spec from file location for plugin '{self._plugin_name}'")
 
             # Create a new module based on the spec
             module = util.module_from_spec(spec)
@@ -56,21 +71,18 @@ class ModulePluginLoader(PluginLoader):
 
             # Load the module by executing it
             sys.modules[self._plugin_name] = module
+
+            if spec.loader is None:
+                raise RuntimeError(f"No loader is specified for spec for plugin '{self._plugin_name}'")
+            
             spec.loader.exec_module(module)
         else:
             module = sys.modules[self._plugin_name]
+        
+        return module
 
-        self._module = module
-
-    def unload(self):
-        self._module = None
-
-    def is_loaded(self) -> bool:
-        return self._module is not None
-    
     def _get_plugin_classes(self) -> list[type[Plugin]]:
         """Retrieve all subclasses of `Plugin` from the loaded module."""
-
         if self._module is None:
             raise RuntimeError("Module not loaded")
 
@@ -81,10 +93,7 @@ class ModulePluginLoader(PluginLoader):
                 plugin_classes.append(obj)
         return plugin_classes
 
-    def create_instance(self) -> Plugin:
-        if not self.is_loaded():
-            raise RuntimeError("Can not create plugin instance of plugin is not loaded")
-        
+    def _find_plugin_class(self) -> type[Plugin]:
         potential_classes = self._get_plugin_classes()
         num_classes = len(potential_classes)
 
@@ -94,4 +103,9 @@ class ModulePluginLoader(PluginLoader):
             class_names = [cls.__name__ for cls in potential_classes]
             raise RuntimeError(f"Only one sub-class of class '{Plugin.__name__}' allowed in plugin '{self._plugin_name}' but found {num_classes}: {class_names}")
         
-        return potential_classes[0]()
+        return potential_classes[0]
+    
+    def create_instance(self) -> Plugin:
+        if self._plugin_class is None:
+            raise RuntimeError(f"Cannot create instance of not-loaded plugin '{self._plugin_name}'")
+        return self._plugin_class()
